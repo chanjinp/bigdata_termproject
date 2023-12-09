5e3c14ef40161a12290e2fc4d627b3ade8a9cf02
Dori <qarkchanjin@gmail.com>
2023-12-09 19:10:41 +0900

    더미 데이터 숙소 10개, 유저 10명, 예약 각 숙소당 3건씩, 후기 각 예약 당 1건씩

--- src/mongodb/faker.js
+++ src/mongodb/faker.js
@@ -1,9 +1,10 @@
 const faker = require("faker");
 const mongoose = require("mongoose");
-const { Accommodation, Guest, Reservation, Review } = require("./models");
+const {Accommodation, Guest, Reservation, Review} = require("./models");
+const {fa} = require("faker/lib/locales");
 
 // eslint-disable-next-line no-undef
-generateDummyData = async (nAccommodation, nGuest, nReservation, nReview) => {
+generateDummyData = async (nAccommodation, nGuest, nReservation) => {
     const accommodations = [];
     const guests = [];
     const reservations = [];
@@ -11,10 +12,69 @@ generateDummyData = async (nAccommodation, nGuest, nReservation, nReview) => {
 
     //dummyComponent
     const accomodationType = ['All', 'All', 'All', 'All', 'All', 'Personal', 'Personal', 'Personal', 'Personal', 'Personal'];
-    const checkIn = new Date('2023-12-01');
-    const checkOut = new Date('2023-12-05');
+    //편의 시설
+    const basic = '화장지, ' +
+        '손과 몸을 씻을 수 있는 비누, ' +
+        '게스트당 수건 1장, ' +
+        '침대당 침구 1세트, ' +
+        '게스트당 베개 1개, ' +
+        '청소용품'
 
+    const guestSearch = '수영장, ' +
+        '와이파이, ' +
+        '주방, ' +
+        '무료 주차 공간, ' +
+        '자쿠지, ' +
+        '세탁기 또는 건조기, ' +
+        '에어컨 또는 난방, ' +
+        '셀프 체크인, ' +
+        '노트북 작업 공간, ' +
+        '반려동물 동반 가능'
 
+    const safety = '일산화탄소 경보기, ' +
+        '화재 경보기, ' +
+        '소화기, ' +
+        '구급상자, ' +
+        '비상 대피 안내도 및 현지 응급 구조기관 번호'
+
+    const access = '계단이나 단차가 없는 현관, ' +
+        '폭 32인치/81cm 이상의 넓은 출입구, ' +
+        '폭 36인치/91cm 이상의 넓은 복도, ' +
+        '휠체어 접근 가능 욕실'
+    const comport = [basic, guestSearch, safety, access]
+    //체크인, 체크아웃 랜덤 생성
+    const generateCheckInDate = () => {
+        return faker.date.between('2023-01-01', '2023-12-31');
+    };
+    const generateCheckOutDate = (checkInDate) => {
+        const maxCheckOutDate = new Date(checkInDate.getTime() + (30 * 24 * 60 * 60 * 1000)); // 예: 30일 이내
+        return faker.date.between(checkInDate, maxCheckOutDate);
+    };
+    //주중, 주말 카운트
+    const countWeekdaysAndWeekends = (checkInDate, checkOutDate) => {
+        let weekdayCount = 0;
+        let weekendCount = 0;
+
+        const currentDate = new Date(checkInDate.getTime());
+
+        while (currentDate <= checkOutDate) {
+            const dayOfWeek = currentDate.getDay(); // 0: 일요일, 1: 월요일, ..., 6: 토요일
+
+            if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+                // 주중 (월요일부터 금요일까지)
+                weekdayCount++;
+            } else {
+                // 주말 (토요일 또는 일요일)
+                weekendCount++;
+            }
+
+            // 다음 날짜로 이동
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+
+        return { weekdayCount, weekendCount };
+    };
+    faker.locale = "ko";
     const db = mongoose.connection.db;
 
     console.log("drop all collections");
@@ -28,55 +88,69 @@ generateDummyData = async (nAccommodation, nGuest, nReservation, nReview) => {
 
     //숙소 10개 등록
     for (let i = 0; i < nAccommodation; i++) {
-        faker.locale = "ko";
+        const bedNum = Math.floor(Math.random() * (10 - 2 + 1)) + 2;
+        const randomIdx = Math.floor(Math.random() * 4);
         accommodations.push(
             new Accommodation({
-                name: faker.lorem.word(),
+                name: faker.lorem.words(),
                 type: accomodationType[i],
-                address: faker.internet.address(),
-                bedroom: faker.number(),
-                bed: 5,
-                bathroom: 2,
-                description: '좋음',
-                comport: '기본,게스트검색,안전,접근성',
-                number: 10,
-                weekdayPrice: 50000,
-                weekendPrice: 80000,
+                address: faker.address.city(),
+                bedroom: bedNum,
+                bed: bedNum,
+                bathroom: Math.floor(Math.random() * (3 - 1 + 1)) + 1,
+                description: faker.commerce.productDescription(),
+                comport: comport[randomIdx],
+                number: bedNum,
+                weekdayPrice: Math.floor((Math.random() + 1) * 3) * 10000,
+                weekendPrice: Math.floor((Math.random() + 1) * 5) * 10000,
             })
         );
     }
     //게스트 등록
-    for (let i = 0; i<nGuest; i++) {
-        faker.locale = "ko";
+    for (let i = 0; i < nGuest; i++) {
         const firstName = faker.name.firstName();
-        const lastName = faker.name.LastName();
-        const fullName = firstName + lastName;
-        guests.push (
-            new Guest( {
+        const lastName = faker.name.lastName();
+        const fullName = lastName + firstName;
+        guests.push(
+            new Guest({
                 name: fullName,
             })
         )
     }
+
     //숙소 당 예약 3개
     accommodations.map(async (accommodation) => {
-        for( let i = 0; i < nReservation; i++) {
+        const checkInDate = generateCheckInDate();
+        const checkOutDate = generateCheckOutDate(checkInDate);
+        const dayCount =  countWeekdaysAndWeekends(checkInDate, checkOutDate)
+        for (let i = 0; i < nReservation; i++) {
+            reviews.push(new Review({
+                star: Math.floor(Math.random() * 5),
+                content: faker.lorem.words()
+            }))
+
             reservations.push(
                 new Reservation({
-                    guest: guests[Math.random()*nGuest],
+                    guest: guests[Math.floor(Math.random() * nGuest)]._id,
                     accommodation: accommodation,
-                    review: null,
-                    totalPrice: 50000  + 80000,
+                    review: reviews[i],
+                    totalPrice: accommodation.weekdayPrice * dayCount.weekdayCount + accommodation.weekendPrice * dayCount.weekendCount,
+                    reservationNum: accommodation.number,
+                    checkIn: checkInDate,
+                    checkOut: checkOutDate,
+                    isCheckOut: true,
                 })
             )
         }
     })
-    //숙소 후기 3개 생성
 
 
     console.log("dummpy data inserting....");
     await Accommodation.insertMany(accommodations);
     await Guest.insertMany(guests);
-};
+    await Reservation.insertMany(reservations);
+    await Review.insertMany(reviews);
+}
 
 // eslint-disable-next-line no-undef
-module.exports = { generateDummyData };
+module.exports = {generateDummyData};
--- src/mongodb/models/review.js
+++ src/mongodb/models/review.js
@@ -3,12 +3,10 @@ const mongoose = require("mongoose");
 
 const ReviewSchema = new Schema(
     {
-        guest: {type:Types.ObjectId , required: true, ref: "Guest"}, //게스트 아이디
-        reservation: {type:Types.ObjectId, required: true, ref: "Reservation"}, //예약 번호
         content: {type:String, required: true}, //리뷰 내용
         star: {type: Number, required: true},// 별점
     },
     {timestamps: true} //mapped Superclass처럼 공통 속성을 묶어주는 것처럼 사용
 )
 const Review = mongoose.model("Review", ReviewSchema);
-module.exports = {Review};
+module.exports = {Review, ReviewSchema};
